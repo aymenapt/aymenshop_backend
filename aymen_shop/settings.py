e3a888c054cb261b080f8de508c44ff4176e4f14
ababsa aymen <aymenababsa1@gmail.com>
2022-09-01 14:00:33 +0100

    update commit

--- aymen_shop/settings.py
+++ aymen_shop/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
