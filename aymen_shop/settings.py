a27b1688192d884d8c941610cf5a0a9a02a7491e
ababsa aymen <aymenababsa1@gmail.com>
2022-09-02 01:13:31 +0100

    update2 commit

--- aymen_shop/settings.py
+++ aymen_shop/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from decouple import  config
 from dj_database_url import parse as dburl
 import os
-import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
