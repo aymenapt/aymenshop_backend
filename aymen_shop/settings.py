eec5367bf2dee4cd40e3edf2dcab1731dc536be0
ababsa aymen <aymenababsa1@gmail.com>
2022-09-02 01:28:39 +0100

    update2 commit

--- aymen_shop/settings.py
+++ aymen_shop/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE= 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
