a833dd555075a6a2aeb019d44ab8d73423078f9f
ababsa aymen <aymenababsa1@gmail.com>
2022-09-02 02:04:26 +0100

    update2 commit

--- product/models.py
+++ product/models.py
@@ -62,33 +62,9 @@ class Product(models.Model):
         return f'/{self.category.slug}/{self.slug}/'
     
     def get_image(self):
-        if self.image:
-            return self.image.url
-        return ''
-    
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return self.thumbnail.url
-        else:
-            if self.image:
-                self.thumbnail = self.make_thumbnail(self.image)
-                self.save()
-
-                return   self.thumbnail.url
-            else:
-                return ''
-    
-    def make_thumbnail(self, image, size=(300, 200)):
-        img = Image.open(image)
-        img.convert('RGB')
-        img.thumbnail(size)
-
-        thumb_io = BytesIO()
-        img.save(thumb_io, 'JPEG', quality=85)
-
-        thumbnail = File(thumb_io, name=image.name)
-
-        return thumbnail
+             return self.image.url
+        
+   
 
 
 class Comment(models.Model):
